90a49b4e6f791cf69b83500d50fd18eacf7bf26e
Brecht Van de Sijpe <brecht.vandesijpe@student.kuleuven.be>
2024-06-14 10:38:03 +0200

    Readme + config + analysis comments

--- Config.py
+++ Config.py
@@ -12,49 +12,54 @@ from Schedulers.CR import CR
 import numpy as np
 import salabim as sim
 
-TIME_LIMIT = 365
+# These parameters are not inteded to be changed. Generally you can use this to set delays in terms of days, hours and minutes.
 MINUTES_IN_HOUR = 60
 HOURS_IN_DAY = 24
 DAYS_IN_WEEK = 7
 
 # Logging parameters
-ENABLE_SIM_TRACE = False
-LOG_QUEUES = True
-LOG_MACHINES = False
-LOG_GENERATOR = False
-LOG_DATAFRAMES =  False
+ENABLE_SIM_TRACE = False                    # Show live information of the simulation by salabim
+LOG_QUEUES = True                           # Print the queue statistics at the end of the simulation
+LOG_MACHINES = False                        # Print machine statistics at the end of the simulation
+LOG_GENERATOR = False                       # Print generator statistics at the end of the simulation
+LOG_DATAFRAMES =  False                     # Print the data we plot in the log.txt file, mostly for debugging purposes
 
 # Generator parameters
-ORDER_INTERVAL_MEAN = DAYS_IN_WEEK * HOURS_IN_DAY * MINUTES_IN_HOUR / 500
-ORDER_INTERVAL_STD = 0
-DEADLINE_MEAN = 2
-DEADLINE_STD = 0.5
-DEADLINE_MIN = 1
+ORDER_INTERVAL_MEAN = DAYS_IN_WEEK * HOURS_IN_DAY * MINUTES_IN_HOUR / 500           # The mean time that the OrderGenerator waits to generate a new order
+ORDER_INTERVAL_STD = 0                                                              # The standarddeviation for the time that the OrderGenerator waits to generate a new order
+                                                                                    # We left this on 0 in order to make this a fixed value. This is however a Noram distribution in the code
+DEADLINE_MEAN = 2                                                                   # Deadline mean in weeks
+DEADLINE_STD = 0.5                                                                  # Deadline standard deviation in weeks
+DEADLINE_MIN = 1                                                                    # Minimum deadline in weeks, if the normaldistribution samples a value below this, this will be the new set value
 
+SHAPE_PARAM = 4                                                                     # Shape parameter for the Gamma-distribution of the transition time
+SCALE_PARAM = 14                                                                    # Schale parameter for the Gamma-distribution of the transition time
 # GUI parameters
 REFRESH_RATE = 10
-SHAPE_PARAM = 4  # Vormparameter (kan worden aangepast)
-SCALE_PARAM = 14  # Schaalparameter (kan worden aangepast)
-SLEEP_FACTOR = 0.0 # Determines silumation speed
-
+SLEEP_FACTOR = 0.0                                                                  # Determines silumation speed, this is a small delay for which the program
+                                                                                    # halts. In case you sue a lot of orders / week, put this number low since the program has a lot to process.
+                                                                                    # In case  you generate a small amount of orders / week you should put this a little higher, in order to make
+                                                                                    # it possible to see the effects of the simulation. Typical values between 0.0 (faster simulation) and 0.5 (slower simulation)
+                                                                                    # DISCLAIMER: This does not change anything to the results of the ismulation, this is only for graphical purposes
 materials = ["FX", "ND", "ND", "NU", "RD", "YL"]
 order_types = list(OrderType)
-order_type_weights = [0.1187, 0.069, 0.0734, 0.3663, 0.2085, 0.0353, 0.1288]
+order_type_weights = [0.1187, 0.069, 0.0734, 0.3663, 0.2085, 0.0353, 0.1288]        # Weights from the pie chart in order to generate the OrderTypes based on frequency in the dataset
 
-can_do_lists = [
-    [OrderType.FX_16m_37xxx, OrderType.NU_20m_80xx, OrderType.NU_22m_68xxx, OrderType.ND_22m_143xxx],
-    [OrderType.NU_22m_68xxx, OrderType.ND_22m_143xxx],
-    [OrderType.ND_35m_143xxx, OrderType.ND_35m_79xxx, OrderType.ND_40m_143xxx],
+can_do_lists = [                                                                                        # Define which machine can do which OrderType     
+    [OrderType.FX_16m_37xxx, OrderType.NU_20m_80xx, OrderType.NU_22m_68xxx, OrderType.ND_22m_143xxx],   # Line 103
+    [OrderType.NU_22m_68xxx, OrderType.ND_22m_143xxx],                                                  # Line 104
+    [OrderType.ND_35m_143xxx, OrderType.ND_35m_79xxx, OrderType.ND_40m_143xxx],                         # Line 105
 ]
 
-priority_lists = [
-    [OrderType.FX_16m_37xxx, OrderType.ND_22m_143xxx, OrderType.NU_20m_80xx, OrderType.NU_22m_68xxx],
-    [OrderType.NU_22m_68xxx, OrderType.ND_22m_143xxx],
-    [OrderType.ND_35m_143xxx, OrderType.ND_35m_79xxx, OrderType.ND_40m_143xxx],
+priority_lists = [                                                                                      # Define the priority list for each machine in case you want to implement Priority scheduling
+    [OrderType.FX_16m_37xxx, OrderType.ND_22m_143xxx, OrderType.NU_20m_80xx, OrderType.NU_22m_68xxx],   # Line 103
+    [OrderType.NU_22m_68xxx, OrderType.ND_22m_143xxx],                                                  # Line 104
+    [OrderType.ND_35m_143xxx, OrderType.ND_35m_79xxx, OrderType.ND_40m_143xxx],                         # Line 105
 ]
 
-configurations = []
-for i in range(3):
+# This part is mainly for the simultion setup, should not be changed in order to set parameters
+configurations = []                                                                                     # This is for the setup of the scheduler, the configuration determines how we decide the runtime and error_rate parameters (by sampling)
+for i in range(len(can_do_lists)):
     configurations.append(Configuration(can_do_lists[i], priority_lists[i]))
     
 order_type_map = {e.name: e for e in OrderType}
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Project software simulation
+
+This project is a simulation of three extrusion lines in the Bossch-factory in Tienen. We consider different scheduling algorithms and provide a simple framework in which new algorithms can be included.
+
+## Adding new algorithms
+
+New algorithms can be added by inheriting the Method-class, located in Method.py . The FCFS examples shows us how:
+
+1. Create a new file in the Schedulers folder, call this the name of your algorithm. The name should be without spaces or special characters. THe program automaticly detects the files and adds them to the model. You can select in the GUI which algorithms you want to compare. It is python code, so you make it end on ".py". In the case of First Come First Serve this is: "FCFS.py".
+
+2. Write the necessary code for the scheduling algorithm.
+
+```
+from Method import *
+
+class FCFS(Method):
+    def __init__(self):
+        super().__init__("FCFS")
+        
+    def schedule_next(self, machine):
+        for order in machine.queue:
+            if order is not None and machine.configuration.can_do_list:
+                machine.queue.remove(order)
+                return order, order.size
+        return None, 0
+```
+
+Comment: In order to let the program correctly detect and use the scheduler, fill in the exact name of the file -".py" into the field of the super().__init__(<name>). The classname should be the same value. The scheduler is linked to a machine, you can access the current orderqueue for this machine via the "queue"-field of the machine. All scheduling logic starts and ends in the schedule_next-method. This method receives the machine it is assigned to and returns the next order to run and the amount of  pieces the machine has to run. This allows the programmer to let the machine process only a part of the order, adding the remaining part back into the queue and thus providing preemption if required. In case you want to process the full order you can return the "size"-field of the order as the second argument. For example SJF would then look like this in SJF.py:
+
+```
+from Method import *
+
+class SJF(Method):
+    def __init__(self):
+        super().__init__("SJF")
+        
+    def schedule_next(self, machine):
+        # Here you put the scheduling code
+        return <the chosen order>, <amount of processed pieces>
+```
+
+3. In order to recognize the new algorithm in the program you have to include it in the "Config.py" file. In order to do this for the FCFS algorithm you can use this line:
+
+```
+from Schedulers.FCFS import FCFS
+```
+
+Comment: in your case you replace the "FCFS" in this line with the name of your algorithm.
+
+## Setting config parameters
+
+In Config.py all parameters can be changed. These include logging paramters, of which most logs can be found in the log.txt file. Logging of the queues is, when enabled, provided in the terminal.
